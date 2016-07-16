69478b0c68f15780c3d55930acb95e7363d5e9ec
markchanner <markchanner@gmail.com>
2016-07-16 21:21:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/AbstractEmoticon.java
@@ -0,0 +1,13 @@
+package com.example.mark.msccomputerscienceproject;
+
+import android.graphics.Bitmap;
+
+/**
+ * @author Mark Channer for Birkbeck MSc Computer Science project
+ */
+public abstract class AbstractEmoticon implements Emoticon {
+
+    public AbstractEmoticon(int arrayX, int arrayY, int emoWidth, int emoHeight, Bitmap bitmap, String emoticonType, int offScreenStartPositionY) {
+
+    }
+}
