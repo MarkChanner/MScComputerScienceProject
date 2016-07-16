4f3a877797c512521a1dcb6da4eec41d2c980575
markchanner <markchanner@gmail.com>
2016-07-16 21:16:17 +0100

    Initial commit

---
+++ app/src/test/java/com/example/mark/msccomputerscienceproject/EmoticonImplTest.java
@@ -0,0 +1,24 @@
+package com.example.mark.msccomputerscienceproject;
+
+import android.graphics.Bitmap;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Mark Channer for Birkbeck MSc Computer Science final project
+ */
+public class EmoticonImplTest {
+
+    @Mock
+    Bitmap bitmap;
+
+
+    @Test
+    public void testRetrieveGamePieceAndShowType() throws Exception {
+        Emoticon emo = new EmoticonImpl(3, 4, 20, 20, bitmap, "HAPPY", 10);
+        assertEquals(emo.getArrayX(), 3);
+    }
+}
