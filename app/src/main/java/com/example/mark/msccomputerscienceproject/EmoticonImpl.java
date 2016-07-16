204378044b19801d8f0de50e6e01c5dd2507edcf
markchanner <markchanner@gmail.com>
2016-07-16 21:24:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/EmoticonImpl.java
@@ -0,0 +1,11 @@
+package com.example.mark.msccomputerscienceproject;
+
+import android.graphics.Bitmap;
+
+public class EmoticonImpl extends AbstractEmoticon {
+
+    public EmoticonImpl(int arrayX, int arrayY, int emoWidth, int emoHeight, Bitmap bitmap, String emoticonType, int offScreenStartPositionY) {
+        super(arrayX, arrayY, emoWidth, emoHeight, bitmap, emoticonType, offScreenStartPositionY);
+    }
+}
+
