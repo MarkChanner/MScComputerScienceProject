3a4e231b722607bb7cd322d6aede83acd53cb3b2
markchanner <markchanner@gmail.com>
2016-07-19 14:23:07 +0100

    Adds code for it to pass the set tests

--- app/src/main/java/com/example/mark/msccomputerscienceproject/EmoticonFactory.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/EmoticonFactory.java
@@ -3,13 +3,15 @@ package com.example.mark.msccomputerscienceproject;
 import android.content.Context;
 import android.graphics.Bitmap;
 
+import java.util.Random;
 
 public class EmoticonFactory {
 
     BitmapCreator bitmapCreator;
 
     public EmoticonFactory(Context context, int emoWidth, int emoHeight) {
-
+        this.bitmapCreator = BitmapCreator.getInstance();
+        this.bitmapCreator.prepareScaledBitmaps(context, emoWidth, emoHeight);
     }
 
     /**
@@ -18,8 +20,33 @@ public class EmoticonFactory {
      * @return a subclass of AbstractEmoticon (AbstractEmoticon implements Emoticon interface)
      */
     public Emoticon createEmoticon(int x, int y, int emoWidth, int emoHeight, int offScreenStartPositionY) {
+        Random random = new Random();
         String emoID = null;
         Bitmap bitmap = null;
+        switch (random.nextInt(5)) {
+            case 0:
+                emoID = "ANGRY";
+                bitmap = bitmapCreator.getAngryBitmap();
+                break;
+            case 1:
+                emoID = "DELIGHTED";
+                bitmap = bitmapCreator.getHappyBitmap();
+                break;
+            case 2:
+                emoID = "EMBARRASSED";
+                bitmap = bitmapCreator.getEmbarrassedBitmap();
+                break;
+            case 3:
+                emoID = "SURPRISED";
+                bitmap = bitmapCreator.getSurprisedBitmap();
+                break;
+            case 4:
+                emoID = "UPSET";
+                bitmap = bitmapCreator.getSadBitmap();
+                break;
+            default:
+                break;
+        }
         return new EmoticonImpl(x, y, emoWidth, emoHeight, bitmap, emoID, offScreenStartPositionY);
     }
 
