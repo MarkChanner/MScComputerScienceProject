f963db1574a9ce2431db13d4319da0fac48bb6a1
markchanner <markchanner@gmail.com>
2016-07-19 14:58:04 +0100

    Passes tests

--- app/src/main/java/com/example/mark/msccomputerscienceproject/SelectionImpl.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/SelectionImpl.java
@@ -2,6 +2,12 @@ package com.example.mark.msccomputerscienceproject;
 
 public class SelectionImpl implements Selection {
 
+    public static final int X = 0;
+    public static final int Y = 1;
+    private int[] selection01 = new int[2];
+    private int[] selection02 = new int[2];
+    private boolean selection01Made;
+
     /**
      * @author Mark Channer for Birkbeck MSc Computer Science project
      */
@@ -16,37 +22,40 @@ public class SelectionImpl implements Selection {
 
     @Override
     public boolean selection01Made() {
-
+        return selection01Made;
     }
 
     @Override
     public int[] getSelection01() {
-
+        return selection01;
     }
 
     @Override
     public void setSelection01(int x, int y) {
-
+        selection01[X] = x;
+        selection01[Y] = y;
+        selection01Made = true;
     }
 
     @Override
     public int[] getSelection02() {
-
+        return selection02;
     }
 
     @Override
     public void setSelection02(int x, int y) {
-
+        selection02[X] = x;
+        selection02[Y] = y;
     }
 
     @Override
     public boolean sameSelectionMadeTwice() {
-
+        return false;
     }
 
     @Override
     public boolean adjacentSelections() {
-
+        return false;
     }
 
     @Override
