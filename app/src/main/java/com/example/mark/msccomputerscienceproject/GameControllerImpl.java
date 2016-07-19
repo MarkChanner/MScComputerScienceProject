5c99a8bad4c4cb7d6a4ea7cf7c57965d82577dd0
markchanner <markchanner@gmail.com>
2016-07-19 22:41:50 +0100

    Initial Commit

--- app/src/main/java/com/example/mark/msccomputerscienceproject/GameControllerImpl.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/GameControllerImpl.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class GameControllerImpl extends Activity implements GameController {
 
+    public static final int X_MAX = 8;
+    public static final int Y_MAX = 7;
+
     /**
      * @author Mark Channer for Birkbeck MSc Computer Science project
      */
