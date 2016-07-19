27f5b2920c2058bb1f3ccc1a8562a18a69044e27
markchanner <markchanner@gmail.com>
2016-07-19 14:52:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/SelectionImpl.java
@@ -0,0 +1,56 @@
+package com.example.mark.msccomputerscienceproject;
+
+public class SelectionImpl implements Selection {
+
+    /**
+     * @author Mark Channer for Birkbeck MSc Computer Science project
+     */
+    public SelectionImpl() {
+
+    }
+
+    @Override
+    public void resetUserSelections() {
+
+    }
+
+    @Override
+    public boolean selection01Made() {
+
+    }
+
+    @Override
+    public int[] getSelection01() {
+
+    }
+
+    @Override
+    public void setSelection01(int x, int y) {
+
+    }
+
+    @Override
+    public int[] getSelection02() {
+
+    }
+
+    @Override
+    public void setSelection02(int x, int y) {
+
+    }
+
+    @Override
+    public boolean sameSelectionMadeTwice() {
+
+    }
+
+    @Override
+    public boolean adjacentSelections() {
+
+    }
+
+    @Override
+    public void setSelection02ToSelection01() {
+
+    }
+}
