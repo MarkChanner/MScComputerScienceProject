36fb2c4a60f6958c714f9cc00f9ed927db2ae829
markchanner <markchanner@gmail.com>
2016-07-19 21:38:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/EmoticonMatchFinder.java
@@ -0,0 +1,26 @@
+package com.example.mark.msccomputerscienceproject;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class EmoticonMatchFinder {
+
+    private Emoticon[][] emoticons;
+
+    public EmoticonMatchFinder(Emoticon[][] emoticons) {
+        this.emoticons = emoticons;
+    }
+
+    public ArrayList<LinkedList<Emoticon>> findVerticalMatches() {
+
+    }
+
+    public ArrayList<LinkedList<Emoticon>> findHorizontalMatches() {
+
+    }
+
+    public boolean anotherMatchPossible() {
+
+    }
+
+}
