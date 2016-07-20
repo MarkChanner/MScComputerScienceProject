26e9fa505f663c26c5515b2d87cd4d402fa79d25
markchanner <markchanner@gmail.com>
2016-07-20 09:18:19 +0100

    Initial commit

---
+++ app/src/test/java/com/example/mark/msccomputerscienceproject/GameModelImplTest.java
@@ -0,0 +1,26 @@
+package com.example.mark.msccomputerscienceproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mark Channer for Birkbeck MSc Computer Science final project
+ */
+public class GameModelImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testHandleSelection() throws Exception {
+
+    }
+}
