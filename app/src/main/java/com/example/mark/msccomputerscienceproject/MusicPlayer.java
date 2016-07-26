915cfb3f338d41bcb65f67d7704bd146297742af
markchanner <markchanner@gmail.com>
2016-07-26 16:54:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/MusicPlayer.java
@@ -0,0 +1,55 @@
+package com.example.mark.msccomputerscienceproject;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class MusicPlayer {
+
+    private final static String TAG = "MusicPlayer";
+    private MediaPlayer mediaPlayer;
+
+    public MusicPlayer(Context context) {
+        mediaPlayer = new MediaPlayer();
+        try {
+            AssetManager assetManager = context.getAssets();
+            AssetFileDescriptor musicDescriptor = assetManager.openFd("shroom_ridge.ogg");
+            mediaPlayer.setDataSource(musicDescriptor.getFileDescriptor(),
+                    musicDescriptor.getStartOffset(), musicDescriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+        } catch (IOException e) {
+            mediaPlayer = null;
+            Log.e(TAG, "IOException in MusicPlayer constructor: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public void startMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+
+    public void pauseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stopMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    public void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
+}
