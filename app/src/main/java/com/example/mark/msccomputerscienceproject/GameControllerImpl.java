448eb4e9b6163082dfd80a6fe8819905c3ccda23
markchanner <markchanner@gmail.com>
2016-07-29 16:19:02 +0100

    Small changes only

--- app/src/main/java/com/example/mark/msccomputerscienceproject/GameControllerImpl.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/GameControllerImpl.java
@@ -40,7 +40,6 @@ public class GameControllerImpl extends Activity implements GameController {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
@@ -53,27 +52,24 @@ public class GameControllerImpl extends Activity implements GameController {
         Point size = new Point();
         Display display = getWindowManager().getDefaultDisplay();
         display.getSize(size);
-
         int sizeX = (size.x - ((screenLayout.getPaddingLeft() * 2) + screenLayout.getPaddingRight()));
         int scoreBoardViewSizeX = (int) (sizeX * 0.1);
         int gameBoardViewSizeX = (int) (sizeX * 0.9);
         int sizeY = (size.y - (screenLayout.getPaddingTop() + screenLayout.getPaddingBottom()));
         this.emoWidth = gameBoardViewSizeX / X_MAX;
         this.emoHeight = sizeY / Y_MAX;
-        this.emoticons = new AbstractEmoticon[X_MAX][Y_MAX];
 
+        this.emoticons = new AbstractEmoticon[X_MAX][Y_MAX];
         BitmapCreator bitmapCreator = BitmapCreator.getInstance();
-        // prepareScaleBitmaps(Context, int, int) MUST be called!!!
         bitmapCreator.prepareScaledBitmaps(this, emoWidth, emoHeight);
         EmoticonCreator emoCreator = new EmoticonCreatorImpl(bitmapCreator, emoWidth, emoHeight);
         GridPopulator populator = new GridPopulatorImpl(emoCreator, emoticons);
         this.gameModel = new GameModelImpl(this, populator, new MatchFinder(), emoticons);
+
         this.scoreBoardView = new ScoreBoardView(this, scoreBoardViewSizeX, sizeY / 3);
         this.gameBoardView = new GameBoardView(this, emoticons, gameBoardViewSizeX, sizeY, emoWidth, emoHeight);
-
         LinearLayout.LayoutParams scoreParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(scoreBoardViewSizeX, sizeY / 3));
         screenLayout.addView(scoreBoardView, scoreParams);
-
         LinearLayout.LayoutParams boardParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(gameBoardViewSizeX, sizeY));
         boardParams.setMargins(screenLayout.getPaddingLeft(), 0, gameBoardViewSizeX, 0);
         screenLayout.addView(gameBoardView, boardParams);
@@ -116,7 +112,6 @@ public class GameControllerImpl extends Activity implements GameController {
 
     @Override
     public void updateEmoticonCoordinates() {
-        //Log.d(TAG, "updateEmoticonCoordinates()");
         gameModel.updateEmoticonSwapCoordinates();
         gameModel.updateEmoticonDropCoordinates();
     }
