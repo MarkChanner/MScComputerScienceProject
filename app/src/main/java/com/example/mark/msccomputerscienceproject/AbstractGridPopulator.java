86977f6f602c05a1dac5b73ec3ef00bde6f1c0eb
markchanner <markchanner@gmail.com>
2016-07-29 12:39:35 +0100

    Initial commit. Moves code from GridPopulatorImpl

--- app/src/main/java/com/example/mark/msccomputerscienceproject/AbstractGridPopulator.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/AbstractGridPopulator.java
@@ -6,20 +6,27 @@ package com.example.mark.msccomputerscienceproject;
  * the start of the game, another emoticon is chosen until one that does not form a match is
  * found { @inheritDocs }
  */
-public class GamePopulatorImpl implements GamePopulator {
+public class AbstractGridPopulator implements GridPopulator {
 
     public static final int X_MAX = GameControllerImpl.X_MAX;
     public static final int Y_MAX = GameControllerImpl.Y_MAX;
     public static final int ROW_START = 0;
     public static final int COLUMN_TOP = 0;
+    private EmoticonCreator emoCreator;
 
-    public void populateBoard(EmoticonCreator emoCreator, Emoticon[][] emoticons) {
+    public AbstractGridPopulator(EmoticonCreator emoCreator, Emoticon[][] emoticons) {
+        this.emoCreator = emoCreator;
+        populateBoard(emoticons);
+    }
+
+    @Override
+    public void populateBoard(Emoticon[][] emoticons) {
         Emoticon newEmoticon;
         for (int x = ROW_START; x < X_MAX; x++) {
             int dropGap = Y_MAX * 2;
             for (int y = COLUMN_TOP; y < Y_MAX; y++) {
                 do {
-                    newEmoticon = emoCreator.generateRandomEmoticon(x, y, ((y - Y_MAX) - dropGap));
+                    newEmoticon = getRandomEmoticon(x, y, ((y - Y_MAX) - dropGap));
 
                 } while ((y >= 2 &&
                         (newEmoticon.getEmoticonType().equals(emoticons[x][y - 1].getEmoticonType()) &&
@@ -33,4 +40,15 @@ public class GamePopulatorImpl implements GamePopulator {
             }
         }
     }
+
+    @Override
+    public Emoticon getRandomEmoticon(int x, int y, int offScreenStartPositionY) {
+        return emoCreator.generateRandomEmoticon(x, y, offScreenStartPositionY);
+    }
+
+    @Override
+    public Emoticon getEmptyEmoticon(int x, int y) {
+        return emoCreator.getEmptyEmoticon(x, y);
+    }
+
 }
