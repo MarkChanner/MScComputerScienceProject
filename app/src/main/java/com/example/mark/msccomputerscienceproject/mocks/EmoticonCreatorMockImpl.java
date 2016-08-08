edad32b6a865e2b7231e41e828f06b3cc5de8837
markchanner <markchanner@gmail.com>
2016-08-08 13:49:00 +0100

    Renames MockEmoticon.java

---
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/mocks/EmoticonCreatorMockImpl.java
@@ -0,0 +1,7 @@
+package com.example.mark.msccomputerscienceproject.mocks;
+
+/**
+ * Created by Mark on 08/08/2016.
+ */
+public class EmoticonCreatorMockImpl {
+}
--- app/src/main/java/com/example/mark/msccomputerscienceproject/mocks/MockEmoticonImpl.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/mocks/MockEmoticonImpl.java
@@ -1,11 +1,14 @@
-package com.example.mark.msccomputerscienceproject.model;
+package com.example.mark.msccomputerscienceproject.mocks;
 
 import android.graphics.Bitmap;
+
+import com.example.mark.msccomputerscienceproject.model.AbstractEmoticon;
+import com.example.mark.msccomputerscienceproject.model.Emoticon;
 
 /**
  * @author Mark Channer for Birkbeck MSc Computer Science project
  */
-public class MockEmoticon extends AbstractEmoticon {
+public class MockEmoticon extends AbstractEmoticon implements Emoticon {
 
     public MockEmoticon(int x, int y, int emoWidth, int emoHeight, Bitmap mockBitmap, String emoticonType) {
         super(x, y, emoWidth, emoHeight, mockBitmap, emoticonType, y);
