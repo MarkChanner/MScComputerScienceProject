93eae4bdb4b903d012d4f823542ebd8a2bf30044
markchanner <markchanner@gmail.com>
2016-08-09 18:30:42 +0100

    Renames EmptyEmoticon class to BlankTile

--- app/src/main/java/com/example/mark/msccomputerscienceproject/model/BlankTile.java
+++ app/src/main/java/com/example/mark/msccomputerscienceproject/model/BlankTile.java
@@ -4,11 +4,11 @@ import android.graphics.Bitmap;
 /**
  * @author Mark Channer for Birkbeck MSc Computer Science project
  */
-public class EmptyEmoticon extends GamePiece {
+public class BlankTile extends GamePiece {
 
     public static final String EMOTION_TYPE = "EMPTY";
 
-    public EmptyEmoticon(int x, int y, int emoWidth, int emoHeight, Bitmap emptyBitmap) {
+    public BlankTile(int x, int y, int emoWidth, int emoHeight, Bitmap emptyBitmap) {
         super(x, y, emoWidth, emoHeight, emptyBitmap, EMOTION_TYPE, y);
         super.dropping = false;
     }
