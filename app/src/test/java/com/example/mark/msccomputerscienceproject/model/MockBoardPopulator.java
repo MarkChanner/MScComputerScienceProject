85969539afb84c9e72c072aafbb844bf875fceef
markchanner <markchanner@gmail.com>
2016-09-17 17:02:26 +0100

    Updates variable names

--- app/src/test/java/com/example/mark/msccomputerscienceproject/model/MockBoardPopulator.java
+++ app/src/test/java/com/example/mark/msccomputerscienceproject/model/MockBoardPopulator.java
@@ -2,26 +2,31 @@ package com.example.mark.msccomputerscienceproject.model;
 
 import android.graphics.Bitmap;
 
+import com.example.mark.msccomputerscienceproject.controller.GameActivityImpl;
+
 import org.mockito.Mock;
 
 /**
- * Extension of BoardPopulatorImpl for testing purposes. Populates a GameBoard with
- * emoticons so that no matches are initially possible. This is done by assigning
- * each emoticon type a unique number to be output when toString() is called.
+ *  Populates a GameBoard witH emoticons so that no matches are initially
+ *  possible. This is done by assigning each emoticon type a unique number
+ *  to be output when toString() is called.
  *
  * @author Mark Channer for Birkbeck MSc Computer Science project
  */
-public class MockBoardPopulator extends BoardPopulatorImpl implements BoardPopulator {
+public class MockBoardPopulator implements BoardPopulator {
 
-    private static final int EMO_WIDTH = 20;
-    private static final int EMO_HEIGHT = 20;
+    public static final int ROWS = GameActivityImpl.MAX_ROWS;
+    public static final int COLUMNS = GameActivityImpl.MAX_COLUMNS;
+    public static final int ROW_START = 0;
+    public static final int COLUMN_TOP = 0;
+    private static final int TILE_WIDTH = 20;
+    private static final int TILE_HEIGHT = 20;
     private GameBoard gameBoard;
 
     @Mock
     Bitmap bitmap;
 
     public MockBoardPopulator(GameBoard gameBoard) {
-        super(gameBoard);
         this.gameBoard = gameBoard;
     }
 
@@ -33,7 +38,7 @@ public class MockBoardPopulator extends BoardPopulatorImpl implements BoardPopul
         for (int x = ROW_START; x < COLUMNS; x++) {
             for (int y = COLUMN_TOP; y < ROWS; y++) {
                 String type = counter <= 9 ? "0" + counter : "" + counter;
-                emoticon = new Emoticon(x, y, EMO_WIDTH, EMO_HEIGHT, bitmap, type, offScreenStartPositionY);
+                emoticon = new Emoticon(x, y, TILE_WIDTH, TILE_HEIGHT, bitmap, type, offScreenStartPositionY);
                 gameBoard.setGamePiece(x, y, emoticon);
                 counter++;
             }
